e994cc7ef74ebdd0a1f59cae9eab2edc601d6abd
shlomi <shl.lev.ks@gmail.com>
2021-03-20 00:52:28 +0200

    add debug config
    add routing crud commands

--- tut/packages/routing-app/router.js
+++ tut/packages/routing-app/router.js
@@ -1,11 +1,58 @@
 const express = require('express')
 const router = express.Router();
-const accounts = require('./db')
+let accounts = require('./db')
+
 // Get request parameters
 router.get("/accounts", (req,res) => {
     res.json({userData: accounts});
 })
 
+//Post
+router.post("/accounts", (req,res) => {
+    const incomingAccount = req.body;
+    accounts.push(incomingAccount);
+    res.json(incomingAccount);
+})
+
+
+router.get('/account/:id', (req,res) => {
+    const accountid = Number(req.params.id);
+    const getAccount = accounts.find( (account) => account.id === accountid);
+    
+    if (!getAccount) {
+        res.status(500).send("Account not found");
+    } else {
+        res.json({userData: [ getAccount]})
+    }
+});
+
+router.put('/account/:id', (req,res) => {
+    const accountid = Number(req.params.id);
+    const body = req.body;
+    const account = accounts.find(account=> accountid === account.id);
+    const index = accounts.indexOf(account);
+
+    if ( !account ) {
+        res.status(500).send("Account not found");
+    } else {
+        const updatedAccount = { ...account, ...body}
+        console.log(account);
+        account[index] = updatedAccount;
+        res.end();
+    }
+});
+
+router.delete('/account/:id', (req,res) => {
+    const accountid = Number(req.params.id);
+    const newAccounts = accounts.filter(account=>  account.id != accountid);
+
+    if ( !newAccounts ) {
+        res.status(500).send("Account not found");
+    } else {
+        accounts = newAccounts;
+        res.send(accounts);
+    }
+});
 
 
 module.exports = router;
--- tut/packages/routing-app/server.js
+++ tut/packages/routing-app/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
-const router = require('./router');
+const bodyParser = require('body-parser');
+
+const route = require('./router');
 const port = 3001;
 
-app.use('/api', router)
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/api', route)
 
 app.get("/", (req, res) => {
     res.end("Routing App");
