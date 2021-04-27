9b7ebba28d89731e3ba2f6af1147d6c55ffa6395
shlomi <shl.lev.ks@gmail.com>
2021-04-27 15:30:56 +0300

    tdd mocha

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, {Application, Request, Response, NextFunction} from 'express';
+import routes from './routes/index';
+export default function createServer() {
+    const app: Application = express();
+
+    app.get('/', (req: Request, res: Response, next: NextFunction) => {
+        res.send("hello world");
+    });
+
+    app.use(routes);
+
+    return app;
+}
---
+++ test/server/server-run.spec.ts
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+import request from "supertest";
+import { expect } from "chai";
+
+import createServer from 'server';
+
+const app = createServer();
+
+describe("server checks", function () {
+    it("server is created without error", function(done) {
+        request(app).get("/").expect(200, done)
+    } );
+});
